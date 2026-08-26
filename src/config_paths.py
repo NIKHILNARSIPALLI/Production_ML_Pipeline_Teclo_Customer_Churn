9878e7d119775d39b7640a2be046a7ab644e38ef
NIKHILNARSIPALLI <100080613+NIKHILNARSIPALLI@users.noreply.github.com>
2026-08-26 22:02:48 +0530

    Started populating config.py

--- src/config_paths.py
+++ src/config_paths.py
@@ -1,6 +1,22 @@
 from dataclasses import dataclass
+from dataclasses import fields
 from pathlib import path
 import yaml
+
+
+#Let's create a function that filters the required fields from the Yaml file
+# such that when using across multiple yaml fields and yaml files that contains
+# more than required parameteres then this filtered values will help not raise error of more/less paramteres
+def create_config(config_class, raw: dict):
+
+    valid_fields = (fields.name for field in fields(config_class))
+    filtered = {
+        key: value for
+        key, value in raw.items()
+        if key in valid_fields
+    }
+    return config_class(**filtered)
+
 
 
 class DatasetConfig:
@@ -9,6 +25,9 @@ class DatasetConfig:
         self.root_dir = root_dir
         self.train_subdir = train_subdir
         self.test_subdir = test_subdir
+
+class LoadData:
+    ...
 
     
 
