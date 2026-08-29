9bb8e032ce0e53a7cd1ce1e892c82b04b7df910a
NIKHILNARSIPALLI <100080613+NIKHILNARSIPALLI@users.noreply.github.com>
2026-08-29 14:24:04 +0530

    Added loggers

--- src/config.py
+++ src/config.py
@@ -4,6 +4,14 @@ from pathlib import Path
 import yaml
 
 
+#Let's set up the logger
+from logger import set_logger
+
+logger = set_logger(__name__)
+
+
+
+
 """
 Let's create a function that filters the required fields from one of the Yaml file key:values pair
 such that when using across multiple yaml fields and yaml files that contains
@@ -12,31 +20,38 @@ So, ideally input would be a yaml_file["Key"], and output would be the value pai
 """
 def create_config(config_class: type, raw: dict, strict=True):
 
-    # We want to make sure the config_class is an actual data class before moving further
-    if not is_dataclass(config_class):
-        raise TypeError(f"{config_class.__name__} must be a dataclass")
+     # We want to make sure the config_class is an actual data class before moving further
+     if not is_dataclass(config_class):
+          logger.critical(f"Provided config_class must be a dataclass {config_class.__name__}")
+          raise TypeError(f"{config_class.__name__} must be a dataclass")
 
 
-    # Let's get all fields from the dataclass and filter the fields that we need to pass to the config_class
-    valid_fields = {(field.name for field in fields(config_class))}
+     # Let's get all fields from the dataclass and filter the fields that we need to pass to the config_class
+     valid_fields = {(field.name for field in fields(config_class))}
 
-    #Before moving futher let's make sure all the value's mentioned in the yaml file
-    # for that specific field have been used up or if we are missing any key parameters
-    unknown = set(raw) - valid_fields
-    if strict and unknown:
+     #Before moving futher let's make sure all the value's mentioned in the yaml file
+     # for that specific field have been used up or if we are missing any key parameters
+     unknown = set(raw) - valid_fields
+     if strict and unknown:
+         logger.critical(f"Unkown parameters found in {config_class.__name__} class: {unknown}")
          raise ValueError(
               f"Unknown paramters for "
               f"{config_class.__name__} : {unknown}"
          )
 
 
-    filtered = {
+     filtered = {
         key: value for
         key, value in raw.items()
         if key in valid_fields
     }
 
-    return config_class(**filtered)
+     if filtered == None:
+         logger.warning(f"Filtered configs for {config_class.__name__} class is empty")
+
+     logger.info(f"Created config class for {config_class.__name__}")
+
+     return config_class(**filtered)
 
 
 
@@ -76,24 +91,30 @@ class TrainingConfig:
 def load_config() -> Configs:
 
     #Main project file oath and relative yaml file path
-    main_dir = Path(__file__).resolve().parent.parent
-    yaml_path = main_dir.joinpath("configs","config.yaml")
+     main_dir = Path(__file__).resolve().parent.parent
+     yaml_path = main_dir.joinpath("configs","config.yaml")
 
-    raw = yaml.safe_load(open(yaml_path))
+     try:
+          raw = yaml.safe_load(open(yaml_path))
+          logger.info(f"Loaded the yaml file from {yaml_path}")
+     except:
+          logger.critical(f"File not found {yaml_path}")
+          raise FileNotFoundError(f"Could not locate {yaml_path}")
+          
 
-    return Configs(
-         dataset = create_config(
-              DatasetConfig, raw["dataset"]
-         ),
+     return Configs(
+          dataset = create_config(
+               DatasetConfig, raw["dataset"]
+          ),
 
-         model = create_config(
-              ModelConfig, raw["model"]
-         ),
+          model = create_config(
+               ModelConfig, raw["model"]
+          ),
 
-         training = create_config(
-              TrainingConfig, raw["train"]
-         )
-    )
+          training = create_config(
+               TrainingConfig, raw["train"]
+          )
+     )
 
 """
 So, now in other python files we can simply call the function load_congig
